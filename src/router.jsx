bf6d24e630f285821d6ad80700bc3ae16f4b7aff
Sarwar <2plus7sarwar@gmail.com>
2023-05-18 01:43:47 +0600

    setup project with react router and tailwindcss

---
+++ src/router.jsx
@@ -0,0 +1,18 @@
+import { createBrowserRouter } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import HomePage from "./pages/Home/HomePage";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainLayout></MainLayout>,
+        children: [
+            {
+                path: '/',
+                element: <HomePage></HomePage>
+            }
+        ]
+    }
+])
+
+export default router;
