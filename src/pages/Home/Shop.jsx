0ff4825894dadec30bda2ba8707a4575a69a7b3c
Sarwar <2plus7sarwar@gmail.com>
2023-05-20 21:10:38 +0600

    fixed responsive issue

--- src/pages/Home/Shop.jsx
+++ src/pages/Home/Shop.jsx
@@ -29,13 +29,13 @@ const Shop = () => {
             <h1 className='text-5xl text-light mb-10'>Shop</h1>
             
             <div>
-                <button className={`${tab=='all'? 'bg-sec text-dark ' : ''} px-4 py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('all')}>All</button>
-                <button className={`${tab=='science'? 'bg-sec text-dark ' : ''} px-4 py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('science')}>Science</button>
-                <button className={`${tab=='math'? 'bg-sec text-dark ' : ''} px-4 py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('math')}>Math</button>
-                <button className={`${tab=='engineering'? 'bg-sec text-dark ' : ''} px-4 py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('engineering')}>Engineering</button>
-                <button className={`${tab=='others'? 'bg-sec text-dark ' : ''} px-4 py-3 border-sec border-2`} onClick={()=>setTab('others')}>Others</button>
+                <button className={`${tab=='all'? 'bg-sec text-dark ' : ''} px-2 md:px-4 py-1 md:py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('all')}>All</button>
+                <button className={`${tab=='science'? 'bg-sec text-dark ' : ''} px-2 md:px-4 py-1 md:py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('science')}>Science</button>
+                <button className={`${tab=='math'? 'bg-sec text-dark ' : ''} px-2 md:px-4 py-1 md:py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('math')}>Math</button>
+                <button className={`${tab=='engineering'? 'bg-sec text-dark ' : ''} px-2 md:px-4 py-1 md:py-3 border-sec border-2 border-r-0`} onClick={()=>setTab('engineering')}>Engineering</button>
+                <button className={`${tab=='others'? 'bg-sec text-dark ' : ''} px-2 md:px-4 py-1 md:py-3 border-sec border-2`} onClick={()=>setTab('others')}>Others</button>
             </div>
-            <hr className='mb-5 border-sec'/>
+           
 
             {/* products */}
             <div className='grid grid-cols-1 lg:grid-cols-2 md:gap-10 gap-5 border-2 border-sec p-5'>
